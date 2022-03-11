deb8c9593598a3c4bbd9dac3538398ea75245a6a
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2022-03-11 11:21:40 +0530

    Update videoplayer.py

--- rose/adv/videoplayer.py
+++ rose/adv/videoplayer.py
@@ -1,23 +1,55 @@
 __MODULE__ = "Video Play"
 __HELP__ = """
 
-Rose can play video in your group's voice chat 
-zero lag time with high quality
+ can play video in your group's voice chat 
+ ✅Play Commands:
 
-× /vplay - `[Youtube URL/Search Query/live URL or reply video/audion]` 
-× /vpause - Pause the playing music on voice chat.
-× /vresume - Resume the paused music on voice chat.
-× /vskip- Skip the current playing music on voice chat
-× /vend or /stop- Stop the playout.
-× /queue- Check queue list.
-× /playplaylist - Start playing Your Saved Playlist.
-× /playlist - Check Your Saved Playlist On Servers.
-× /delmyplaylist- Delete any saved music in your playlist
-× /delgroupplaylist- Delete any saved music in your group's playlist [Requires Admin Rights.]
+cplay or cstream stands for channel play.
+vplay stands for video play.
 
-This is most advance music player in telegram base on private Socure 
-music player repo luna x dN can play music or video in your group video chat without lag...!
+/play or /vplay or /cplay  - Bot will start playing your given query on voice chat.
 
+/stream or /cstream - Stream live links on voice chats.
 
+/channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
+
+✅Bot's Server Playlists:
+/playlist  - Check Your Saved Playlist On Servers.
+/deleteplaylist - Delete any saved music in your playlist
+/play  - Start playing Your Saved Playlist from Servers.
+✅Extra  Commands:
+/start - Start the Music Bot.
+/help  - Get Commands Helper Menu with detailed explanations of commands.
+/ping- Ping the Bot and check Ram, Cpu etc stats of Bot.
+
+✅Group Settings:
+/settings - Get a complete group's settings with inline buttons
+
+🔗 Options in Settings:
+
+1️⃣ You can set Audio Quality you want to stream on voice chat.
+
+2️⃣ You can set Video Quality you want to stream on voice chat.
+
+3️⃣ Auth Users:- You can change admin commands mode from here to everyone or admins only. If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
+
+4️⃣ Clean Mode: When enabled deletes the bot's messages after 5 mins from your group to make sure your chat remains clean and good.
+
+5️⃣ Command Clean : When activated, Bot will delete its executed commands (/play, /pause, /shuffle, /stop etc) immediately.
+
+6️⃣ Play Settings:
+
+/playmode - Get a complete play settings panel with buttons where you can set your group's play settings. 
+
+Options in playmode:
+
+1️⃣ Search Mode [ Direct or Inline] - Changes your search mode while you give /play mode. 
+
+2️⃣ Play Mode [ Group or Channel] - Changes your Play mode to channel or group and streams music there only.
+
+3️⃣ Play Type [ Everyone or Admins] - If admins, only admins present in group can play music on voice chat.
+
+MUSIC PLAYER ALL CREDIT ONLY GOES TO
+@TeamYukki | @TheYukki | @YukkiSupport
  """
 __advtools__ = __HELP__
